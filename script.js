8921b659f58427516cffc31d5a449f002ec1df2e
praneethguptha <123263232+hemanthkotha1198@users.noreply.github.com>
2023-07-19 20:14:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function office() {
+    document.getElementById('loc').src="https://images.unsplash.com/photo-1579487785973-74d2ca7abdd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b2ZmaWNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60"
+}
+function Home() {
+    document.getElementById('loc').src="https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG9tZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60"
+}
+function Zyme() {
+    document.getElementById('loc').src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR82GGyUH2BkQ9Gf6mT6yfYgG7625bK6vlWhw&usqp=CAU"
+}
