6b2d7c557ce66ac40e2a9f946c06d8f802dbbb62
praneethguptha <123263232+hemanthkotha1198@users.noreply.github.com>
2023-07-19 20:10:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulbOff() {
+    document.getElementById("bulb").src="https://media.istockphoto.com/id/1400469622/photo/led-filament-tungsten-vintage-light-bulb-isolated-on-white-background.webp?b=1&s=170667a&w=0&k=20&c=ajnYFhAFAie_tlAhjeEKnPbMP5l30o6XWIKavConQJw=";
+}
+function bulbOn() {
+    document.getElementById("bulb").src="https://m.media-amazon.com/images/I/51bCZTw1HlL._AC_UF1000,1000_QL80_.jpg";
+}
