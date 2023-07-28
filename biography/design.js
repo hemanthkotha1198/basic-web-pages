18f7c632cfc3aa8c80eed6adfcba8fabe7ab330e
praneethguptha <123263232+hemanthkotha1198@users.noreply.github.com>
2023-07-28 21:28:55 +0530

    Add files via upload

---
+++ biography/design.js
@@ -0,0 +1,13 @@
+// ReactDOM.render(<h1>this is a react page.</h1>,document.getElementById("content"));
+ReactDOM.render(<div>
+  <h1>M.S.Dhoni</h1>
+  <img src="dhoni.jpeg"/>
+  <p Style="color:red">
+  MS Dhoni is a former Indian cricketer and captain of the Indian national team. He is widely regarded as one of the greatest cricketing captains of all time, having led India to victory in the 2007 ICC World Twenty20, the 2010 and 2016 Asia Cups, and the 2011 ICC Cricket World Cup.
+        Dhoni was born on July 7, 1981 in Ranchi, Bihar (now in Jharkhand), and made his debut for the Indian cricket team in December 2004. He is known for his aggressive and innovative captaincy, as well as his calm and composed demeanor on the field.
+        Off the field, Dhoni is an avid sports enthusiast and enjoys playing football and badminton. He is also a licensed pilot and has a passion for flying.
+      
+  </p>
+  </div>
+  ,document.getElementById("content"));
+
